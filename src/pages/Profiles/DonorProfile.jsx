da0163787b15f46d286e2c0d21e14606d63052b1
Alireja-khan <alirejakhan18@gmail.com>
2025-07-02 02:06:01 +0600

    change the Your Profile and Donor Profile page UI little bit. like bg change and title poloshing so on.

--- src/pages/Profiles/DonorProfile.jsx
+++ src/pages/Profiles/DonorProfile.jsx
@@ -6,11 +6,8 @@ import { TbCircleCheckFilled } from 'react-icons/tb';
 import { myAddFoodsPromise } from '../../services/myAddFoodsApi';
 
 const DonorProfile = ({ food }) => {
-
     const [foodsData, setFoodsData] = useState([]);
     const location = useLocation();
-
-
 
     useEffect(() => {
         const fetchData = async () => {
@@ -18,93 +15,94 @@ const DonorProfile = ({ food }) => {
             setFoodsData(data);
         };
 
-        fetchData();
-    }, []);
-
+        if (food?.userEmail) fetchData();
+    }, [food?.userEmail]);
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-5 my-6">
+        <div className="px-4 pb-35 sm:px-6 lg:px-8">
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-5 text-center relative z-10">
                 <motion.button
                     whileHover={{ scale: 1.25 }}
-                    whileTap={{ scale: 0.95 }}
-                    onHoverStart={() => console.log('hover started!')}
+                    whileTap={{ scale: 1.15 }}
                 >
-                    <Link>
-                        <BsPersonFill className="w-13 h-13" />
+                    <Link to="/profile">
+                        <BsPersonFill className="w-12 h-12 text-lime-500 mx-auto" />
                     </Link>
                 </motion.button>
+
+                <p className="text-3xl sm:text-4xl md:text-5xl font-extrabold">Donor</p>
 
                 <span className="relative group rounded px-5 py-2 font-semibold text-black overflow-hidden text-3xl">
                     <motion.span
                         className="absolute bottom-0 left-0 right-0 bg-[#bee8b1] z-0"
-                        initial={{ height: '100%' }}
-                        animate={{ height: ['100%', '70%', '100%'] }}
+                        initial={{ height: "100%" }}
+                        animate={{ height: ["100%", "70%", "100%"] }}
                         transition={{
                             duration: 3,
                             repeat: Infinity,
-                            repeatType: 'mirror',
-                            ease: 'easeInOut',
+                            repeatType: "mirror",
+                            ease: "easeInOut",
                         }}
                     ></motion.span>
-                    <span className="relative z-10">Donor Profile</span>
+                    <span className="relative z-10 text-4xl">Profile</span>
                 </span>
             </div>
+
+            <motion.p
+                className="text-gray-600 text-lg text-center max-w-2xl mx-auto"
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                viewport={{ once: false, amount: 0.3 }}
+            >
+                View and explore the donor’s profile including their contributions and contact details.
+            </motion.p>
 
             <div className="max-w-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
                 <div className="px-4 py-8">
                     <div className="shadow-2xl rounded-xl border overflow-hidden hover:shadow-2xl transition duration-300">
-                        <div className="w-full h-full">
-                            {food.donorImage && (
-                                <img
-                                    src={food.donorImage}
-                                    alt="Profile"
-                                    className="w-full h-auto object-cover"
-                                />
-                            )}
-                        </div>
+                        {food?.donorImage && (
+                            <img
+                                src={food.donorImage}
+                                alt="Profile"
+                                className="w-full h-auto object-cover"
+                            />
+                        )}
                     </div>
                 </div>
 
-
-
                 <div>
-
-                    {/* Information */}
-
-                    <div className="px-6 py-4 mb-5 shadow-2xl border flex flex-col justify-center">
+                    <div className="px-6 bg-white py-4 mb-5 shadow-2xl border flex flex-col justify-center">
                         <p className="text-green-500 text-lg text-center mb-3">Informations</p>
 
-                        <p className="text-base flex justify-between text-gray-800 mb-1">
+                        <p className="text-base flex justify-between text-gray-800 mb-2">
                             <span className="font-semibold">Donor:</span>
-                            <span>{food.donorName}</span>
+                            <span>{food?.donorName}</span>
                         </p>
-                        <p className="text-base flex justify-between text-gray-600 mb-1">
+                        <p className="text-base flex justify-between text-gray-600 mb-2">
                             <span className="font-semibold">Donor Email:</span>
-                            <span>{food.userEmail}</span>
+                            <span>{food?.userEmail}</span>
                         </p>
                         <p className="text-base flex justify-between text-gray-600">
-                            <span className="font-semibold">Donor Location:</span>
-                            <span>{food.pickupLocation}</span>
+                            <span className="font-semibold">Pickup Location:</span>
+                            <span>{food?.pickupLocation}</span>
                         </p>
                     </div>
 
-
-
-
-
-                    {/* Donations */}
-
-                    <div className="px-6 py-4 border flex flex-col shadow-2xl justify-center">
+                    <div className="px-6 bg-white py-4 border flex flex-col shadow-2xl justify-center">
                         <p className="text-green-500 text-lg text-center mb-3">Donations</p>
 
                         <div className="flex justify-between gap-4 flex-wrap">
                             <div>
-                                {foodsData.map((food) => (
-                                    <Link to={`/foods/${food._id}`} state={{ from: 'food', back: location.pathname }} key={food._id}>
+                                {foodsData.map((item) => (
+                                    <Link
+                                        to={`/foods/${item._id}`}
+                                        state={{ from: 'donor', back: location.pathname }}
+                                        key={item._id}
+                                    >
                                         <p className="hover:font-semibold flex justify-between items-center gap-3">
-                                            {food.foodName}
-                                            {food.status === 'available' && (
+                                            {item.foodName}
+                                            {item.status === 'available' && (
                                                 <TbCircleCheckFilled className="text-blue-500" />
                                             )}
                                         </p>
@@ -113,8 +111,8 @@ const DonorProfile = ({ food }) => {
                             </div>
 
                             <div>
-                                {foodsData.map((food) => (
-                                    <p key={food._id}>{food.quantity}</p>
+                                {foodsData.map((item) => (
+                                    <p key={item._id}>{item.quantity}</p>
                                 ))}
                             </div>
                         </div>
@@ -122,12 +120,10 @@ const DonorProfile = ({ food }) => {
                         <p className="mt-3 flex justify-between text-green-500">
                             <span>Total Quantity:</span>
                             <span>
-                                {foodsData.reduce((sum, food) => sum + Number(food.quantity), 0)}
+                                {foodsData.reduce((sum, item) => sum + Number(item.quantity), 0)}
                             </span>
                         </p>
                     </div>
-
-
                 </div>
             </div>
         </div>
--- src/pages/Profiles/MyProfile.jsx
+++ src/pages/Profiles/MyProfile.jsx
@@ -27,33 +27,45 @@ const MyProfile = () => {
 
     return (
 
-        <div className="px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col sm:flex-row justify-center items-center gap-5 my-6">
+        <div className="px-4 pt-15 pb-35 bg-green-50 sm:px-6 lg:px-8">
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-5 text-center relative z-10">
                 <motion.button
                     whileHover={{ scale: 1.25 }}
-                    whileTap={{ scale: 0.95 }}
-                    onHoverStart={() => console.log('hover started!')}
+                    whileTap={{ scale: 1.15 }}
+                    onHoverStart={() => console.log("hover started!")}
                 >
-                    <Link>
-                        <BsPersonFill className="w-13 h-13" />
+                    <Link to="/profile">
+                        <BsPersonFill className="w-12 h-12 text-lime-500 mx-auto" />
                     </Link>
                 </motion.button>
+
+                <p className="text-3xl sm:text-4xl md:text-5xl font-extrabold">Your</p>
 
                 <span className="relative group rounded px-5 py-2 font-semibold text-black overflow-hidden text-3xl">
                     <motion.span
                         className="absolute bottom-0 left-0 right-0 bg-[#bee8b1] z-0"
-                        initial={{ height: '100%' }}
-                        animate={{ height: ['100%', '70%', '100%'] }}
+                        initial={{ height: "100%" }}
+                        animate={{ height: ["100%", "70%", "100%"] }}
                         transition={{
                             duration: 3,
                             repeat: Infinity,
-                            repeatType: 'mirror',
-                            ease: 'easeInOut',
+                            repeatType: "mirror",
+                            ease: "easeInOut",
                         }}
                     ></motion.span>
-                    <span className="relative z-10">Profile</span>
+                    <span className="relative z-10 text-4xl">Profile</span>
                 </span>
             </div>
+
+            <motion.p
+                className="text-gray-600 text-lg text-center max-w-2xl mx-auto "
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                viewport={{ once: false, amount: 0.3 }}
+            >
+                View and manage your profile information including your donations, food requests, and personal details.
+            </motion.p>
 
             <div className="max-w-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 items-center">
                 <div className="px-4 py-8">
@@ -71,7 +83,7 @@ const MyProfile = () => {
                 </div>
 
                 <div>
-                    <div className="px-6 py-4 mb-5 shadow-2xl border flex flex-col justify-center">
+                    <div className="px-6 bg-white py-4 mb-5 shadow-2xl border flex flex-col justify-center">
                         <p className="text-green-500 text-lg text-center mb-3">Informations</p>
 
                         <p className="text-base flex justify-between text-gray-800 mb-2">
@@ -84,7 +96,7 @@ const MyProfile = () => {
                         </p>
                     </div>
 
-                    <div className="px-6 py-4 border flex flex-col shadow-2xl justify-center">
+                    <div className="px-6 bg-white py-4 border flex flex-col shadow-2xl justify-center">
                         <p className="text-green-500 text-lg text-center mb-3">Donations</p>
 
                         <div className="flex justify-between gap-4 flex-wrap">
